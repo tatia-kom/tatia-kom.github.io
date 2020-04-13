707442b800f22acf387684ddebea42cf905c717f
tatia-kom <komarovats93@gmail.com>
2020-04-13 22:34:22 +0300

    scroll header

--- js/scripts.js
+++ js/scripts.js
@@ -61,6 +61,15 @@ $(document).ready(function() {
         ]
     });
 
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 0) {
+            $('.header').addClass('header--scroll');
+        }
+        else {
+            $('.header').removeClass('header--scroll');
+        }
+    });
+
     // map
 
     initMap();
